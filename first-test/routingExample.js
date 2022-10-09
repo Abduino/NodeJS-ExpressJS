e58440d0a38004fe2d33b24fa12b32a0d3d6a7fc
Abduino <abdrehmanredi0@gmail.com>
2022-10-09 16:24:23 -0300

    routing example added

--- first-test/routingExample.js
+++ first-test/routingExample.js
@@ -1,14 +1,18 @@
-
 const express = require("express");
 const app=express();
 
-app.get('/',(req,res) => {
+app.get('/hello',(req,res) => {
     console.log("/ is running ");
 
     res.send("<h1>well come to jam rock 2</h1>");
 });
+app.post('/hello',(req,res) => {
+    console.log("/ is running ");
+
+    res.send("<h1>well come to jam rock 2</h1>");
+});
+
 const port = 3000;
 app.listen(port,() => {
     console.log("example listening");
 });
-
